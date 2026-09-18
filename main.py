4c2a9f7c375f0274414de3af11c01a96cdfeebeb
Kirsten Imam 3 <imam.kj@obmontessori.edu.ph>
2026-09-18 08:57:27 +0800

    weqe

--- main.py
+++ main.py
@@ -1,5 +1,31 @@
-from pyscript import document, display
+from pyscript import display, document
 
-coffee = document.getElementById("coffee")
+def create_order(e):
+    subtotal = 0
 
-coffee_price = float(coffee.value)
+    if document.getElementById("item1").checked:
+        subtotal += 120 #+120 on the subtotal value
+
+    if document.getElementById("item2").checked:
+        subtotal += 150 #+150 on the subtotal value
+
+    if document.getElementById("item3").checked:
+        subtotal += 180 #+180 on the subtotal value
+
+    if document.getElementById("item4").checked:
+        subtotal += 168 #+168 on the subtotal value
+
+    if document.getElementById("item5").checked:
+        subtotal += 130 #+130 on the subtotal value
+
+    vat = subtotal * 0.12 #12% of subtotal
+    total = subtotal + vat
+
+    display(f"Subtotal: ₱{subtotal:.2f}", target="show")
+    display(f"VAT: ₱{vat:.2f}", target="show")
+    display(f"Total Amount: ₱{total:.2f}", target="show")
+
+
+    #.checked --> value included in computaiton IF user ticked it (https://www.w3schools.com/jsref/prop_checkbox_checked.asp)
+
+    #.2f = 2 decimal places (https://www.w3schools.com/python/trypython.asp?filename=demo_fstring_modifier_novar)
